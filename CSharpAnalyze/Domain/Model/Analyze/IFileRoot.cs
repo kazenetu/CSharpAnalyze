87ca694b5b570626c2564ab9d652615c9e15e6d2
kazenetu <rakudafactory@gmail.com>
2018-12-09 17:31:55 +0900

    ファイルルートクラスを追加

---
+++ CSharpAnalyze/Domain/Model/Analyze/IFileRoot.cs
@@ -0,0 +1,26 @@
+﻿using CSharpAnalyze.Domain.Model.Analyze.Items;
+using System.Collections.Generic;
+
+namespace CSharpAnalyze.Domain.Model.Analyze
+{
+  /// <summary>
+  /// ファイルルート インターフェース
+  /// </summary>
+  public interface IFileRoot
+  {
+    /// <summary>
+    /// ファイルパス
+    /// </summary>
+    string FilePath { get; }
+
+    /// <summary>
+    /// 外部参照のクラス名とファイルパスのリスト
+    /// </summary>
+    Dictionary<string, string> OtherFiles { get; }
+
+    /// <summary>
+    /// 子メンバ
+    /// </summary>
+    List<IAnalyzeItem> Members { get; }
+  }
+}

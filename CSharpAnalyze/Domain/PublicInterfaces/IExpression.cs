c381af9333c237f1f95df56396b66b55aa992e4c
kazenetu <rakudafactory@gmail.com>
2018-12-19 22:37:55 +0900

    外部公開用interface作成

---
+++ CSharpAnalyze/Domain/PublicInterfaces/IExpression.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CSharpAnalyze.Domain.PublicInterfaces
+{
+  /// <summary>
+  /// Expression ValueObject Interface
+  /// </summary>
+  public interface IExpression: IEquatable<IExpression>
+  {
+    /// <summary>
+    /// 名前
+    /// </summary>
+    string Name { get; }
+
+    /// <summary>
+    /// 変数型名
+    /// </summary>
+    string TypeName { get; }
+
+    /// <summary>
+    /// 比較
+    /// </summary>
+    /// <returns>比較結果</returns>
+    bool Equals(object obj);
+
+    /// <summary>
+    /// ハッシュ値取得
+    /// </summary>
+    /// <returns>ハッシュ値</returns>
+    int GetHashCode();
+  }
+}

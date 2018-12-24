1d42b16e4a9d214cedbcc084e59637dd91afbc8d
kazenetu <rakudafactory@gmail.com>
2018-12-24 15:43:35 +0900

    IOperation解析用スーパークラスを追加

---
+++ CSharpAnalyze/Domain/Model/Analyze/Operations/AbstractOperation.cs
@@ -0,0 +1,16 @@
+﻿using CSharpAnalyze.Domain.PublicInterfaces;
+using System.Collections.Generic;
+
+namespace CSharpAnalyze.Domain.Model.Analyze.Operations
+{
+  /// <summary>
+  /// Operation系のスーパークラス
+  /// </summary>
+  internal abstract class AbstractOperation
+  {
+    /// <summary>
+    /// Expression ValueObject
+    /// </summary>
+    public List<IExpression> Expressions { get; } = new List<IExpression>();
+  }
+}

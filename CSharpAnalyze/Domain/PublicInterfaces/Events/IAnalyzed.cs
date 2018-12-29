cf9b519f42a375d7263e0b36501eeb4ca5e391cf
kazenetu <rakudafactory@gmail.com>
2018-12-30 08:06:46 +0900

    解析完了イベントの外部公開用インターフェースを追加

---
+++ CSharpAnalyze/Domain/PublicInterfaces/Events/IAnalyzed.cs
@@ -0,0 +1,18 @@
+﻿namespace CSharpAnalyze.Domain.PublicInterfaces.Events
+{
+  /// <summary>
+  /// 解析完了イベント インターフェース
+  /// </summary>
+  public interface IAnalyzed : IEvent
+  {
+    /// <summary>
+    /// ファイル名
+    /// </summary>
+    string FilePath { get; }
+
+    /// <summary>
+    /// 解析情報ルート
+    /// </summary>
+    IFileRoot FileRoot { get; }
+  }
+}

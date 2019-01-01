058ec1c880dd1349a34d9da4b46f557cf7ff6871
kazenetu <rakudafactory@gmail.com>
2019-01-01 22:30:01 +0900

    Operation:Binary追加

---
+++ CSharpAnalyze/Domain/Model/Analyze/Operations/Binary.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.CodeAnalysis.Operations;
+
+namespace CSharpAnalyze.Domain.Model.Analyze.Operations
+{
+  /// <summary>
+  /// Operation:Binary
+  /// </summary>
+  internal class Binary : AbstractOperation
+  {
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    /// <param name="operation">IOperationインスタンス</param>
+    public Binary(IBinaryOperation operation)
+    {
+      Expressions.AddRange(OperationFactory.GetExpressionList(operation.LeftOperand));
+
+      var operatorName = string.Empty;
+      switch (operation.OperatorKind)
+      {
+        case BinaryOperatorKind.Add:
+          operatorName = "+";
+          break;
+        case BinaryOperatorKind.And:
+          operatorName = "&";
+          break;
+        case BinaryOperatorKind.ConditionalAnd:
+          operatorName = "&&";
+          break;
+        case BinaryOperatorKind.ConditionalOr:
+          operatorName = "||";
+          break;
+        case BinaryOperatorKind.Divide:
+          operatorName = "/";
+          break;
+        case BinaryOperatorKind.Equals:
+          operatorName = "==";
+          break;
+        case BinaryOperatorKind.ExclusiveOr:
+          operatorName = "^";
+          break;
+        case BinaryOperatorKind.GreaterThan:
+          operatorName = ">";
+          break;
+        case BinaryOperatorKind.GreaterThanOrEqual:
+          operatorName = ">=";
+          break;
+        case BinaryOperatorKind.LessThan:
+          operatorName = "<";
+          break;
+        case BinaryOperatorKind.LessThanOrEqual:
+          operatorName = "<=";
+          break;
+        case BinaryOperatorKind.NotEquals:
+          operatorName = "!=";
+          break;
+        case BinaryOperatorKind.Or:
+          operatorName = "|";
+          break;
+        case BinaryOperatorKind.Multiply:
+          operatorName = "*";
+          break;
+      }
+      if (!string.IsNullOrEmpty(operatorName))
+      {
+        Expressions.Add(new Expression(operatorName, string.Empty));
+      }
+
+      Expressions.AddRange(OperationFactory.GetExpressionList(operation.RightOperand));
+    }
+  }
+}

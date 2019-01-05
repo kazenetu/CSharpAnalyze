efbe700349c18a3a56790c6be0edb3eed1f7cc09
kazenetu <rakudafactory@gmail.com>
2019-01-05 22:34:59 +0900

    不要になったRaiseOtherFileReferencedメソッドを削除

--- CSharpAnalyze/Domain/Model/Analyze/Items/AbstractItem.cs
+++ CSharpAnalyze/Domain/Model/Analyze/Items/AbstractItem.cs
@@ -89,27 +89,5 @@ namespace CSharpAnalyze.Domain.Model.Analyze.Items
                             Where(item => !string.IsNullOrEmpty(item));
       Comments.AddRange(targerComments);
     }
-
-    /// <summary>
-    /// 外部ファイル参照イベント発行
-    /// </summary>
-    /// <param name="targetNode">対象Node</param>
-    /// <param name="targetSymbol">比較対象のSymbol</param>
-    protected void RaiseOtherFileReferenced(SyntaxNode targetNode, ISymbol targetSymbol)
-    {
-      if (!targetSymbol.DeclaringSyntaxReferences.Any())
-      {
-        // ファイルパスなしでイベント送信
-        EventContainer.Raise(new OtherFileReferenced(string.Empty, targetSymbol.Name));
-      }
-
-      var targetNodeFilePath = targetNode.SyntaxTree.FilePath;
-      var ReferenceFilePaths = targetSymbol.DeclaringSyntaxReferences.Select(item => item.SyntaxTree.FilePath).Where(filePath => filePath != targetNodeFilePath);
-      foreach (var referenceFilePath in ReferenceFilePaths)
-      {
-        // ファイルパスありでイベント送信
-        EventContainer.Raise(new OtherFileReferenced(referenceFilePath, targetSymbol.Name));
-      }
-    }
   }
 }

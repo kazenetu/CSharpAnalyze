5a7f7167d67fae1cd8dc50fa66ddaf7971fa12cb
kazenetu <rakudafactory@gmail.com>
2019-01-05 09:40:29 +0900

    イベント発行クラス追加

---
+++ CSharpAnalyze/Domain/Event/RaiseEvents.cs
@@ -0,0 +1,35 @@
+﻿using CSharpAnalyze.Domain.Event.Analyze;
+using Microsoft.CodeAnalysis;
+using System.Linq;
+
+namespace CSharpAnalyze.Domain.Event
+{
+  /// <summary>
+  /// イベント発行クラス
+  /// </summary>
+  internal static class RaiseEvents
+  {
+    /// <summary>
+    /// 外部ファイル参照イベント発行
+    /// </summary>
+    /// <param name="targetNode">対象Node</param>
+    /// <param name="targetSymbol">比較対象のSymbol</param>
+    public static void RaiseOtherFileReferenced(SyntaxNode targetNode, ISymbol targetSymbol)
+    {
+      if (!targetSymbol.DeclaringSyntaxReferences.Any())
+      {
+        // ファイルパスなしでイベント送信
+        EventContainer.Raise(new OtherFileReferenced(string.Empty, targetSymbol.Name));
+      }
+
+      var targetNodeFilePath = targetNode.SyntaxTree.FilePath;
+      var ReferenceFilePaths = targetSymbol.DeclaringSyntaxReferences.Select(item => item.SyntaxTree.FilePath).Where(filePath => filePath != targetNodeFilePath);
+      foreach (var referenceFilePath in ReferenceFilePaths)
+      {
+        // ファイルパスありでイベント送信
+        EventContainer.Raise(new OtherFileReferenced(referenceFilePath, targetSymbol.Name));
+      }
+    }
+
+  }
+}

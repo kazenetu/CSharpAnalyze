e669b1c04a10e2b9ce1d84a6ead0f6ed08408d06
kazenetu <rakudafactory@gmail.com>
2019-01-06 18:08:27 +0900

    プロパティ参照式の処理を見直し

--- CSharpAnalyze/Domain/Model/Analyze/Operations/PropertyReference.cs
+++ CSharpAnalyze/Domain/Model/Analyze/Operations/PropertyReference.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.CodeAnalysis.Operations;
+﻿using CSharpAnalyze.Domain.Event;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Operations;
 
 namespace CSharpAnalyze.Domain.Model.Analyze.Operations
 {
@@ -13,18 +15,63 @@ namespace CSharpAnalyze.Domain.Model.Analyze.Operations
     /// <param name="operation">IOperationインスタンス</param>
     public PropertyReference(IPropertyReferenceOperation operation)
     {
-      if (operation.Instance is ILocalReferenceOperation local)
+      // プロパティ参照情報を追加
+      if (!AddExpressions(operation.Instance))
       {
-        Expressions.Add(new Expression(local.Local.Name, local.Local.Type.Name));
+        AddExpressions(operation);
+      }
+      else
+      {
+        Expressions.Add(new Expression(operation.Property.ContainingSymbol.Name, 
+                            Expression.GetSymbolTypeName(operation.Property.ContainingSymbol)));
+      }
+    }
+
+    /// <summary>
+    /// プロパティ参照情報を追加する
+    /// </summary>
+    /// <param name="operation">IOperationインスタンス</param>
+    /// <returns>追加した場合はtrue nullの場合はfalse</returns>
+    private bool AddExpressions(IOperation operation)
+    {
+      // operationがnullの場合はfalseを返して終了
+      if (operation == null)
+      {
+        return false;
+      }
+
+      // プロパティ参照情報の追加
+      if (operation is IPropertyReferenceOperation prop)
+      {
+        if (prop.Property.IsStatic)
+        {
+          Expressions.Add(new Expression(prop.Property.ContainingSymbol.Name, 
+                              Expression.GetSymbolTypeName(prop.Property.ContainingSymbol)));
+          Expressions.Add(new Expression(".", string.Empty));
+
+          // 外部参照チェック
+          if (prop.Property.ContainingSymbol is INamedTypeSymbol)
+          {
+            // 外部ファイル参照イベント発行
+            RaiseEvents.RaiseOtherFileReferenced(prop.Syntax, prop.Property.ContainingSymbol);
+          }
+        }
+        else
+        {
+          Expressions.Add(new Expression("this", Expression.GetSymbolTypeName(prop.Instance.Type)));
+          Expressions.Add(new Expression(".", string.Empty));
+        }
+
+        Expressions.Add(new Expression(prop.Property.Name, Expression.GetSymbolTypeName(prop.Type)));
         Expressions.Add(new Expression(".", string.Empty));
       }
       else
       {
-        Expressions.Add(new Expression(operation.Property.ContainingSymbol.Name, operation.Property.ContainingSymbol.Kind.ToString()));
+        Expressions.AddRange(OperationFactory.GetExpressionList(operation));
         Expressions.Add(new Expression(".", string.Empty));
       }
 
-      Expressions.Add(new Expression(operation.Property.Name, operation.Property.Type.Name));
+      return true;
     }
   }
 }

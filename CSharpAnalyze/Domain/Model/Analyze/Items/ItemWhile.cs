ea75ab4ad932c5845eb4c1a99d95784ca365f066
kazenetu <rakudafactory@gmail.com>
2019-01-14 22:59:33 +0900

    WhileStatementSyntax用クラスを追加

---
+++ CSharpAnalyze/Domain/Model/Analyze/Items/ItemWhile.cs
@@ -0,0 +1,95 @@
+﻿using CSharpAnalyze.Domain.PublicInterfaces;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharpAnalyze.Domain.Model.Analyze.Items
+{
+  /// <summary>
+  /// アイテム：繰り返し
+  /// </summary>
+  internal class ItemWhile : AbstractItem
+  {
+    /// <summary>
+    /// 条件
+    /// </summary>
+    private List<IExpression> Conditions = new List<IExpression>();
+
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    /// <param name="node">対象Node</param>
+    /// <param name="target">対象ソースのsemanticModel</param>
+    /// <param name="parent">親IAnalyzeItem</param>
+    public ItemWhile(WhileStatementSyntax node, SemanticModel semanticModel, IAnalyzeItem parent) : base(parent, node, semanticModel)
+    {
+      ItemType = ItemTypes.MethodStatement;
+
+      // 条件設定
+      var condition = semanticModel.GetOperation(node.Condition);
+      Conditions.AddRange(OperationFactory.GetExpressionList(condition));
+
+      // 内部処理設定
+      var block = node.Statement as BlockSyntax;
+      foreach (var statement in block.Statements)
+      {
+        Members.Add(ItemFactory.Create(statement, semanticModel, this));
+      }
+
+    }
+    #region 基本インターフェース実装：メソッド
+
+    /// <summary>
+    /// 文字列取得
+    /// </summary>
+    /// <param name="index">前スペース数</param>
+    /// <returns>文字列</returns>
+    public override string ToString(int index = 0)
+    {
+      var result = new StringBuilder();
+      var indexSpace = string.Concat(Enumerable.Repeat("  ", index));
+
+      foreach (var comment in Comments)
+      {
+        result.Append(indexSpace);
+        result.AppendLine($"{comment}");
+      }
+
+      result.Append(indexSpace);
+      result.Append("while(");
+      for (var i = 0; i < Conditions.Count; i++)
+      {
+        var isSetSpace = true;
+        if (i == Conditions.Count - 1)
+        {
+          isSetSpace = false;
+        }
+        else if (Conditions[i].Name == "." || Conditions[i + 1].Name == ".")
+        {
+          isSetSpace = false;
+        }
+        result.Append($"{Conditions[i].Name}");
+        if (isSetSpace)
+        {
+          result.Append(" ");
+        }
+      }
+      result.AppendLine(")");
+
+      result.Append(indexSpace);
+      result.AppendLine("{");
+      foreach (var statement in Members)
+      {
+        result.AppendLine(statement.ToString(index + 1));
+      }
+      result.Append(indexSpace);
+      result.AppendLine("}");
+
+      return result.ToString();
+    }
+
+    #endregion
+  }
+}

8ac4e44c5101cc9286dec036ff82eca8b0b00e9e
kazenetu <rakudafactory@gmail.com>
2019-01-17 22:13:15 +0900

    不要なusingを削除

--- CSharpAnalyze/Domain/Model/Analyze/Items/ItemReturn.cs
+++ CSharpAnalyze/Domain/Model/Analyze/Items/ItemReturn.cs
@@ -2,7 +2,6 @@
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Operations;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;

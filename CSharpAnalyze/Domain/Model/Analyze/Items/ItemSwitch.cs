6f64915e1abcc9c78771c8cb7c0f06a5ab263df6
kazenetu <rakudafactory@gmail.com>
2019-01-20 15:06:51 +0900

    Switch,SwichCase用クラスを追加

---
+++ CSharpAnalyze/Domain/Model/Analyze/Items/ItemSwitch.cs
@@ -0,0 +1,112 @@
+﻿using CSharpAnalyze.Domain.Event;
+using CSharpAnalyze.Domain.PublicInterfaces;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Operations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharpAnalyze.Domain.Model.Analyze.Items
+{
+  /// <summary>
+  /// アイテム：Switch
+  /// </summary>
+  internal class ItemSwitch : AbstractItem
+  {
+    /// <summary>
+    /// 条件
+    /// </summary>
+    public List<IExpression> Conditions { get; } = new List<IExpression>();
+
+    /// <summary>
+    /// Caseリスト
+    /// </summary>
+    public List<IAnalyzeItem> Cases { get; } = new List<IAnalyzeItem>();
+
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    /// <param name="node">対象Node</param>
+    /// <param name="target">対象ソースのsemanticModel</param>
+    /// <param name="parent">親IAnalyzeItem</param>
+    public ItemSwitch(SwitchStatementSyntax node, SemanticModel semanticModel, IAnalyzeItem parent) : base(parent, node, semanticModel)
+    {
+      ItemType = ItemTypes.MethodStatement;
+
+      var operation = semanticModel.GetOperation(node) as ISwitchOperation;
+
+      // 条件設定
+      Conditions.AddRange(OperationFactory.GetExpressionList(operation.Value));
+
+      // Caseリスト設定
+      foreach(var item in operation.Cases)
+      {
+        Cases.Add(ItemFactory.Create(item.Syntax,semanticModel,this));
+      }
+
+    }
+
+    #region 基本インターフェース実装：メソッド
+
+    /// <summary>
+    /// 文字列取得
+    /// </summary>
+    /// <param name="index">前スペース数</param>
+    /// <returns>文字列</returns>
+    public override string ToString(int index = 0)
+    {
+      var result = new StringBuilder();
+      var indexSpace = string.Concat(Enumerable.Repeat("  ", index));
+
+      foreach (var comment in Comments)
+      {
+        result.Append(indexSpace);
+        result.AppendLine($"{comment}");
+      }
+
+      foreach (var modifier in Modifiers)
+      {
+        result.Append(indexSpace);
+        result.Append($"{modifier} ");
+      }
+
+      // 条件
+      result.Append(indexSpace);
+      result.Append("switch(");
+      for (var i = 0; i < Conditions.Count; i++)
+      {
+        var isSetSpace = true;
+        if (i == Conditions.Count - 1)
+        {
+          isSetSpace = false;
+        }
+        else if (Conditions[i].Name == "." || Conditions[i + 1].Name == ".")
+        {
+          isSetSpace = false;
+        }
+        result.Append($"{Conditions[i].Name}");
+        if (isSetSpace)
+        {
+          result.Append(" ");
+        }
+      }
+      result.AppendLine(")");
+
+      // ラベル
+      result.Append(indexSpace);
+      result.AppendLine("{");
+      foreach (var caseItem in Cases)
+      {
+        result.Append(caseItem.ToString(index + 1));
+      }
+      result.Append(indexSpace);
+      result.AppendLine("}");
+
+      return result.ToString();
+    }
+
+    #endregion
+  }
+}
---
+++ CSharpAnalyze/Domain/Model/Analyze/Items/ItemSwitchCase.cs
@@ -0,0 +1,118 @@
+﻿using CSharpAnalyze.Domain.PublicInterfaces;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Operations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharpAnalyze.Domain.Model.Analyze.Items
+{
+  /// <summary>
+  /// アイテム：SwitchCase
+  /// </summary>
+  internal class ItemSwitchCase : AbstractItem
+  {
+    /// <summary>
+    /// Caseラベルリスト
+    /// </summary>
+    public List<List<IExpression>> Labels { get; } = new List<List<IExpression>>();
+
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    /// <param name="node">対象Node</param>
+    /// <param name="target">対象ソースのsemanticModel</param>
+    /// <param name="parent">親IAnalyzeItem</param>
+    public ItemSwitchCase(SwitchSectionSyntax node, SemanticModel semanticModel, IAnalyzeItem parent) : base(parent, node, semanticModel)
+    {
+      ItemType = ItemTypes.MethodStatement;
+
+      var operation = semanticModel.GetOperation(node) as ISwitchCaseOperation;
+
+      // Caseラベル設定
+      foreach (var item in operation.Clauses.Where(item => !(item is IDefaultCaseClauseOperation)))
+      {
+        Labels.Add(OperationFactory.GetExpressionList(item.Children.First()));
+      }
+      foreach(var item in operation.Clauses.Where(item => item is IDefaultCaseClauseOperation))
+      {
+        // TODO defaultを扱えるようにする
+        Labels.Add(OperationFactory.GetExpressionList(item));
+      }
+
+      // 内部処理設定
+      foreach (var statement in node.Statements)
+      {
+        var item = ItemFactory.Create(statement, semanticModel, this);
+
+        if(!(item is null))
+        {
+          Members.Add(ItemFactory.Create(statement, semanticModel, this));
+        }
+      }
+    }
+
+    #region 基本インターフェース実装：メソッド
+
+    /// <summary>
+    /// 文字列取得
+    /// </summary>
+    /// <param name="index">前スペース数</param>
+    /// <returns>文字列</returns>
+    public override string ToString(int index = 0)
+    {
+      var result = new StringBuilder();
+      var indexSpace = string.Concat(Enumerable.Repeat("  ", index));
+
+      foreach (var comment in Comments)
+      {
+        result.Append(indexSpace);
+        result.AppendLine($"{comment}");
+      }
+
+      foreach (var modifier in Modifiers)
+      {
+        result.Append(indexSpace);
+        result.Append($"{modifier} ");
+      }
+
+      // Caseラベル
+      foreach(var label in Labels)
+      {
+        result.Append(indexSpace);
+        result.Append("case ");
+
+        for (var i = 0; i < label.Count; i++)
+        {
+          var isSetSpace = true;
+          if (i == label.Count - 1)
+          {
+            isSetSpace = false;
+          }
+          else if (label[i].Name == "." || label[i + 1].Name == ".")
+          {
+            isSetSpace = false;
+          }
+          result.Append($"{label[i].Name}");
+          if (isSetSpace)
+          {
+            result.Append(" ");
+          }
+        }
+
+        result.AppendLine(":");
+      }
+
+      // 内部処理
+      foreach (var statement in Members)
+      {
+        result.AppendLine(statement.ToString(index + 1));
+      }
+
+      return result.ToString();
+    }
+
+    #endregion
+  }
+}

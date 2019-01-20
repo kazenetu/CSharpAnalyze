4f6446a52c0efdad2f46853f9af140a4b55de0c2
kazenetu <rakudafactory@gmail.com>
2019-01-21 00:20:53 +0900

    配列要素アクセスクラスを追加

---
+++ CSharpAnalyze/Domain/Model/Analyze/Operations/ArrayElementReference.cs
@@ -0,0 +1,34 @@
+﻿using CSharpAnalyze.Domain.Event;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Operations;
+
+namespace CSharpAnalyze.Domain.Model.Analyze.Operations
+{
+  /// <summary>
+  /// Operation:ArrayElementReference 
+  /// </summary>
+  internal class ArrayElementReference : AbstractOperation
+  {
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    /// <param name="operation">IOperationインスタンス</param>
+    public ArrayElementReference(IArrayElementReferenceOperation operation)
+    {
+      // 型名
+      Expressions.AddRange(OperationFactory.GetExpressionList(operation.ArrayReference));
+
+      // 要素取得
+      Expressions.Add(new Expression("[", string.Empty));
+      for (var i = 0; i < operation.Indices.Length; i++)
+      {
+        Expressions.AddRange(OperationFactory.GetExpressionList(operation.Indices[i]));
+        if (i >= 0 && i < operation.Indices.Length - 1)
+        {
+          Expressions.Add(new Expression(",", string.Empty));
+        }
+      }
+      Expressions.Add(new Expression("]", string.Empty));
+    }
+  }
+}

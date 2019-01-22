c746961d142da39cc43092c70e284ef460191f92
kazenetu <rakudafactory@gmail.com>
2019-01-23 00:57:20 +0900

    For分葉変数宣言用クラスを追加

---
+++ CSharpAnalyze/Domain/Model/Analyze/Operations/VariableDeclarator.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Operations;
+
+namespace CSharpAnalyze.Domain.Model.Analyze.Operations
+{
+  /// <summary>
+  /// Operation:VariableDeclarator
+  /// </summary>
+  internal class VariableDeclarator : AbstractOperation
+  {
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    /// <param name="operation">IOperationインスタンス</param>
+    public VariableDeclarator(IVariableDeclaratorOperation operation)
+    {
+      Expressions.Add(new Expression(operation.Symbol.Name,Expression.GetSymbolTypeName(operation.Symbol)));
+
+      if (!(operation.Initializer is null))
+      {
+        Expressions.Add(new Expression("=", string.Empty));
+        Expressions.AddRange(OperationFactory.GetExpressionList(operation.Initializer.Value));
+      }
+    }
+  }
+}

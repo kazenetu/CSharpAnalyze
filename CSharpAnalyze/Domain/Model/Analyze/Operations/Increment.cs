6a8d4f853f3bc05a43b0e2e0d5a9767410348a08
kazenetu <rakudafactory@gmail.com>
2019-01-23 00:56:05 +0900

    インクリメント・デクリメント用クラスを追加

---
+++ CSharpAnalyze/Domain/Model/Analyze/Operations/Increment.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Operations;
+
+namespace CSharpAnalyze.Domain.Model.Analyze.Operations
+{
+  /// <summary>
+  /// Operation:IIncrementOrDecrement
+  /// </summary>
+  internal class Increment : AbstractOperation
+  {
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    /// <param name="operation">IOperationインスタンス</param>
+    public Increment(IIncrementOrDecrementOperation operation)
+    {
+      // インクリメント・デクリメント対象インスタンス
+      Expressions.AddRange(OperationFactory.GetExpressionList(operation.Target));
+
+      // インクリメント・デクリメント
+      switch (operation.Kind)
+      {
+        case OperationKind.Increment:
+          Expressions.Add(new Expression("++", operation.Kind.ToString()));
+          break;
+        case OperationKind.Decrement:
+          Expressions.Add(new Expression("--", operation.Kind.ToString()));
+          break;
+      }
+    }
+  }
+}

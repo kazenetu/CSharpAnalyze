f5ebaedf597a18e6303a822072e5cff1fbef8732
kazenetu <rakudafactory@gmail.com>
2019-01-26 20:52:24 +0900

    不要なusingを削除

--- CSharpAnalyze/Domain/Model/Analyze/Items/ItemFor.cs
+++ CSharpAnalyze/Domain/Model/Analyze/Items/ItemFor.cs
@@ -2,7 +2,6 @@
 using CSharpAnalyze.Domain.PublicInterfaces;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
-using Microsoft.CodeAnalysis.Operations;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;

3832c7a9594390b8da9734aeabe9bc3baa6a613c
kazenetu <rakudafactory@gmail.com>
2019-01-28 20:46:25 +0900

    ItemProperty用インターフェースを追加

--- CSharpAnalyze/Domain/Model/Analyze/Items/ItemProperty.cs
+++ CSharpAnalyze/Domain/Model/Analyze/Items/ItemProperty.cs
@@ -1,5 +1,6 @@
 ﻿using CSharpAnalyze.Domain.Event;
 using CSharpAnalyze.Domain.PublicInterfaces;
+using CSharpAnalyze.Domain.PublicInterfaces.AnalyzeItems;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using System.Collections.Generic;
@@ -11,7 +12,7 @@ namespace CSharpAnalyze.Domain.Model.Analyze.Items
   /// <summary>
   /// アイテム：プロパティ
   /// </summary>
-  internal class ItemProperty : AbstractItem
+  internal class ItemProperty : AbstractItem, IItemProperty
   {
     /// <summary>
     /// プロパティの型リスト
---
+++ CSharpAnalyze/Domain/PublicInterfaces/AnalyzeItems/IItemProperty.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace CSharpAnalyze.Domain.PublicInterfaces.AnalyzeItems
+{
+  /// <summary>
+  /// インターフェース:プロパティ
+  /// </summary>
+  public interface IItemProperty : IAnalyzeItem
+  {
+    /// <summary>
+    /// プロパティの型リスト
+    /// </summary>
+    List<IExpression> PropertyTypes { get; }
+
+    /// <summary>
+    /// デフォルト設定リスト
+    /// </summary>
+    List<IExpression> DefaultValues { get; }
+
+    /// <summary>
+    /// アクセサリスト
+    /// </summary>
+    List<string> AccessorList { get; }
+  }
+}

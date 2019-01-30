f7ae84876fed2085e44ee815ce52c7e50f5b44da
kazenetu <rakudafactory@gmail.com>
2019-01-30 22:44:29 +0900

    ローカル変数用インターフェースを追加

--- CSharpAnalyze/Domain/Model/Analyze/Items/ItemStatementLocalDeclaration.cs
+++ CSharpAnalyze/Domain/Model/Analyze/Items/ItemStatementLocalDeclaration.cs
@@ -1,5 +1,6 @@
 ﻿using CSharpAnalyze.Domain.Event;
 using CSharpAnalyze.Domain.PublicInterfaces;
+using CSharpAnalyze.Domain.PublicInterfaces.AnalyzeItems;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
@@ -12,7 +13,7 @@ namespace CSharpAnalyze.Domain.Model.Analyze.Items
   /// <summary>
   /// アイテム：ローカル変数
   /// </summary>
-  internal class ItemStatementLocalDeclaration : AbstractItem
+  internal class ItemStatementLocalDeclaration : AbstractItem, IItemStatementLocalDeclaration
   {
     /// <summary>
     /// 型リスト
---
+++ CSharpAnalyze/Domain/PublicInterfaces/AnalyzeItems/IItemStatementLocalDeclaration.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace CSharpAnalyze.Domain.PublicInterfaces.AnalyzeItems
+{
+  /// <summary>
+  /// インターフェース:ローカル変数
+  /// </summary>
+  public interface IItemStatementLocalDeclaration : IAnalyzeItem
+  {
+    /// <summary>
+    /// 型リスト
+    /// </summary>
+    List<IExpression> Types { get; }
+
+    /// <summary>
+    /// デフォルト設定リスト
+    /// </summary>
+    List<IExpression> DefaultValues { get; }
+
+    /// <summary>
+    /// 型推論か否か
+    /// </summary>
+    bool IsVar { get; }
+  }
+}

86d503a8f346deb9535c477f43e5eac5cb70cb94
kazenetu <rakudafactory@gmail.com>
2019-02-06 20:20:19 +0900

    ItemForEach用インターフェースを追加

---
+++ CSharpAnalyze/Domain/PublicInterfaces/AnalyzeItems/IItemForEach.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace CSharpAnalyze.Domain.PublicInterfaces.AnalyzeItems
+{
+  /// <summary>
+  /// インターフェース:foreach
+  /// </summary>
+  public interface IItemForEach : IAnalyzeItem
+  {
+    /// <summary>
+    /// ローカル
+    /// </summary>
+    List<IExpression> Local { get; }
+
+    /// <summary>
+    /// コレクション
+    /// </summary>
+    List<IExpression> Collection { get; }
+  }
+}

6fdd2b6de9273431f2412b66cddeee6dae699860
kazenetu <rakudafactory@gmail.com>
2019-02-12 20:52:46 +0900

    テスト用FileRepository追加

---
+++ CSharpAnalyzeTest/Common/CSFileRepositoryMock.cs
@@ -0,0 +1,45 @@
+﻿using CSharpAnalyze.Domain.PublicInterfaces.Repository;
+using System.Collections.Generic;
+
+namespace CSharpAnalyzeTest.Common
+{
+  /// <summary>
+  /// テスト用FileRepository
+  /// </summary>
+  public class CSFileRepositoryMock : ICSFileRepository
+  {
+    /// <summary>
+    /// ファイルリスト
+    /// </summary>
+    private List<(string relativePath, string source)> files = new List<(string relativePath, string source)>();
+
+    /// <summary>
+    /// ファイルリスト取得
+    /// </summary>
+    /// <param name="rootPath">対象ファイルルート(未使用)</param>
+    /// <param name="exclusionKeywords">除外ファイルリスト(未使用)</param>
+    /// <returns>ファイルリスト</returns>
+    public List<(string relativePath, string source)> GetCSFileList(string rootPath, List<string> exclusionKeywords)
+    {
+      return files;
+    }
+
+    /// <summary>
+    /// ファイル情報追加
+    /// </summary>
+    /// <param name="filePath">ファイル名</param>
+    /// <param name="source">ソースコード</param>
+    public void Add(string filePath,string source)
+    {
+      files.Add((filePath, source));
+    }
+
+    /// <summary>
+    /// ファイル情報クリア
+    /// </summary>
+    public void Clear()
+    {
+      files.Clear();
+    }
+  }
+}

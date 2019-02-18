d96cfac9c65b9ca1edbe29e8063802d1733da2a0
kazenetu <rakudafactory@gmail.com>
2019-02-19 00:52:51 +0900

    テスト用ファイル情報クラスを追加

---
+++ CSharpAnalyzeTest/Common/FileData.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpAnalyzeTest.Common
+{
+  /// <summary>
+  /// ファイルデータ
+  /// </summary>
+  public class FileData
+  {
+    #region プロパティ
+
+    /// <summary>
+    /// ファイルパス
+    /// </summary>
+    public string FilePath { get; }
+
+    /// <summary>
+    /// 追加using
+    /// </summary>
+    public string AddUsing { get; }
+
+    /// <summary>
+    /// ソースコード
+    /// </summary>
+    public string Source { get; }
+
+    #endregion
+
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    /// <param name="filePath">ファイルパス</param>
+    /// <param name="addUsing">追加using</param>
+    /// <param name="source">ソースコード</param>
+    public FileData(string filePath, string addUsing, string source)
+    {
+      FilePath = filePath;
+      AddUsing = addUsing;
+      Source = source;
+    }
+  }
+}

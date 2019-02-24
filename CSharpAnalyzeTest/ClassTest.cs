f244f13e51b7a9893aea732588ae93f7662aa8b1
kazenetu <rakudafactory@gmail.com>
2019-02-24 15:31:23 +0900

    クラス継承のテストを追加

--- CSharpAnalyzeTest/ClassTest.cs
+++ CSharpAnalyzeTest/ClassTest.cs
@@ -2,6 +2,7 @@
 using CSharpAnalyze.Domain.PublicInterfaces.AnalyzeItems;
 using CSharpAnalyzeTest.Common;
 using System;
+using System.Linq;
 using System.Text;
 using Xunit;
 
@@ -37,6 +38,14 @@ namespace CSharpAnalyzeTest
           filePath = "Test.cs";
 
           source.AppendLine("public class ClassTest");
+          source.AppendLine("{");
+          source.AppendLine("}");
+          break;
+
+        case CreatePattern.SubClass:
+          filePath = "SubClass.cs";
+
+          source.AppendLine("public class SubClass : ClassTest");
           source.AppendLine("{");
           source.AppendLine("}");
           break;
@@ -102,6 +111,62 @@ namespace CSharpAnalyzeTest
       // 解析実行
       CSAnalyze.Analyze(string.Empty, Files);
     }
-   
+
+    [Fact(DisplayName = "SubClass")]
+    public void SubClassTest()
+    {
+      // スーパークラスを追加
+      CreateFileData(CreateSource(CreatePattern.Standard), null);
+
+      // テストコードを追加
+      CreateFileData(CreateSource(CreatePattern.SubClass), (ev) =>
+         {
+           // ファイル名の確認
+           Assert.True(ev.FilePath == "SubClass.cs");
+
+           // 解析結果の存在確認
+           Assert.NotNull(ev.FileRoot);
+
+           // 外部参照の存在確認
+           Assert.True(ev.FileRoot.OtherFiles.Count == 1);
+           Assert.True(ev.FileRoot.OtherFiles.First().Key == "ClassTest");
+           Assert.True(ev.FileRoot.OtherFiles.First().Value == "Test.cs");
+
+           // 解析結果の件数確認
+           Assert.True(ev.FileRoot.Members.Count == 1);
+
+           // IItemClassインスタンスの確認
+           Assert.True(ev.FileRoot.Members[0] is IItemClass);
+
+           // IItemClassインスタンスを取得
+           var itemClass = ev.FileRoot.Members[0] as IItemClass;
+
+           // スーパークラスの設定確認
+           Assert.True(itemClass.SuperClass.Count == 1);
+           Assert.True(itemClass.SuperClass.First().Name == "ClassTest");
+
+           // 親の存在確認
+           Assert.Null(itemClass.Parent);
+
+           // クラス名の確認
+           Assert.True(itemClass.Name == "SubClass");
+
+           // スコープ修飾子の件数確認
+           Assert.True(itemClass.Modifiers.Count == 1);
+
+           // スコープ修飾子の内容確認
+           Assert.Contains("public", itemClass.Modifiers);
+
+           // ItemTypeの確認
+           Assert.True(itemClass.ItemType == ItemTypes.Class);
+
+           // クラス内の要素の存在確認
+           Assert.True(itemClass.Members.Count == 0);
+         });
+
+      // 解析実行
+      CSAnalyze.Analyze(string.Empty, Files);
+    } 
+
   }
 }

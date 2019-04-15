d99720bdd4d1dd62aa154566d7270df8b5c5ae8b
kazenetu <rakudafactory@gmail.com>
2019-04-16 00:05:13 +0900

    テストスケルトンコードを追加

---
+++ CSharpAnalyzeTest/ReturnTest.cs
@@ -0,0 +1,171 @@
+﻿using CSharpAnalyze.Domain.PublicInterfaces;
+using CSharpAnalyze.Domain.PublicInterfaces.AnalyzeItems;
+using CSharpAnalyzeTest.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace CSharpAnalyzeTest
+{
+  // HACK テスト実装
+  [Trait("Returnのテスト", nameof(ReturnTest))]
+  public class ReturnTest : TestBase
+  {
+    /// <summary>
+    /// ソース作成パターン
+    /// </summary>
+    private enum CreatePattern
+    {
+      Standard,
+    }
+
+    /// <summary>
+    /// ファイル名、ソースコード取得処理
+    /// </summary>
+    /// <param name="pattern">生成パターン</param>
+    /// <returns>ファイルパスとソースコード</returns>
+    private FileData CreateSource(CreatePattern pattern)
+    {
+      var usingList = new StringBuilder();
+      var source = new List<string>();
+      var filePath = string.Empty;
+
+      switch (pattern)
+      {
+        case CreatePattern.Standard:
+          filePath = "Standard.cs";
+
+          source.Add("void target()");
+          source.Add("{");
+          source.Add("}");
+          break;
+      }
+
+      // ソースコード作成
+      var targetSource = new StringBuilder();
+      targetSource.AppendLine("public class Standard");
+      targetSource.AppendLine("{");
+      targetSource.AppendLine("  public void TestMethod()");
+      targetSource.AppendLine("  {");
+
+      source.ForEach(line => targetSource.AppendLine($"    {line }"));
+
+      targetSource.AppendLine("  }");
+      targetSource.AppendLine("}");
+
+      return new FileData(filePath, usingList.ToString(), targetSource.ToString());
+    }
+
+    /// <summary>
+    /// Setup
+    /// </summary>
+    public ReturnTest() : base()
+    {
+      System.Diagnostics.Debug.WriteLine($"Setup {Environment.CurrentDirectory}");
+    }
+
+    /// <summary>
+    /// 基本的なテスト
+    /// </summary>
+    //[Fact(DisplayName = "Standard")]
+    public void StandardArgsTest()
+    {
+      // テストコードを追加
+      CreateFileData(CreateSource(CreatePattern.Standard), (ev) =>
+      {
+        // IItemClassインスタンスを取得
+        var itemClass = GetClassInstance(ev, "Standard.cs");
+
+        // 対象インスタンスのリストを取得
+        var targetInstances = GetTargetInstances(itemClass);
+
+        // 対象の親インスタンスを取得
+        Assert.Single(targetInstances);
+        var targetParentInstance = targetInstances.First() as IItemMethod;
+
+        // 対象インスタンスを取得
+        var targetInstance = GetTargetInstances(targetParentInstance).First() as IItemLocalFunction;
+
+        // 型タイプの確認
+        Assert.Equal("void", GetExpressions(targetInstance.MethodTypes));
+
+        // 外部参照の存在確認
+        Assert.Empty(ev.FileRoot.OtherFiles);
+
+        // パラメータの確認
+        var expectedArgs = new List<(string name, string expressions, string refType, string defaultValue)>()
+        {
+        };
+        Assert.Equal(expectedArgs.Count, GetMemberCount(targetInstance, expectedArgs));
+
+        // 内部処理の確認
+        Assert.Empty(targetInstance.Members);
+      });
+
+      // 解析実行
+      CSAnalyze.Analyze(string.Empty, Files);
+    }
+
+    /// <summary>
+    /// 対象インスタンスの取得
+    /// </summary>
+    /// <param name="itemClass">対象のアイテムクラス</param>
+    /// <returns>対象インスタンスリスト</returns>
+    private List<IItemMethod> GetTargetInstances(IItemClass itemClass)
+    {
+      return itemClass.Members.Where(member => member is IItemMethod).
+              Select(member => member as IItemMethod).ToList();
+    }
+
+    /// <summary>
+    /// 対象インスタンスの取得
+    /// </summary>
+    /// <param name="itemClass">対象のアイテムメソッド</param>
+    /// <returns>対象インスタンスリスト</returns>
+    private List<IItemLocalFunction> GetTargetInstances(IItemMethod itemMethod)
+    {
+      return itemMethod.Members.Where(member => member is IItemLocalFunction).
+              Select(member => member as IItemLocalFunction).ToList();
+    }
+
+    /// <summary>
+    /// メンバー数を取得
+    /// </summary>
+    /// <param name="targetInstance">対象のインスタンス</param>
+    /// <param name="expectedArgs">パラメータの期待値</param>
+    /// <returns>条件が一致するメンバー数</returns>
+    private int GetMemberCount(IItemLocalFunction targetInstance, List<(string name, string expressions, string refType, string defaultValue)> expectedArgs)
+    {
+      // パラメータ数の確認
+      Assert.Equal(expectedArgs.Count, targetInstance.Args.Count);
+
+      // パラメータの確認
+      var argCount = 0;
+      foreach (var (name, expressions, refType, defaultValue) in expectedArgs)
+      {
+        var actualArgs = targetInstance.Args
+                        .Where(arg => arg.name == name)
+                        .Where(arg => GetExpressions(arg.expressions) == expressions)
+                        .Where(arg => string.Concat(arg.modifiers) == refType)
+                        .Where(arg => GetExpressions(arg.defaultValues) == defaultValue);
+        if (actualArgs.Any())
+        {
+          argCount++;
+        }
+      }
+      return argCount;
+    }
+
+    /// <summary>
+    /// パラメーターリストを文字列に変換する
+    /// </summary>
+    /// <param name="expressions">パラメータリスト</param>
+    /// <returns></returns>
+    private string GetExpressions(List<IExpression> expressions)
+    {
+      return string.Concat(expressions.Select(expression => expression.Name));
+    }
+  }
+}

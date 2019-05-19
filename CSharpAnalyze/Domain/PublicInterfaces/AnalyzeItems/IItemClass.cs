05b5646d7acea3c3c9d94859acce4cba78d7943f
kazenetu <rakudafactory@gmail.com>
2019-05-19 17:44:07 +0900

    継承元のプロパティとメソッドのリストを追加

--- CSharpAnalyze/Domain/PublicInterfaces/AnalyzeItems/IItemClass.cs
+++ CSharpAnalyze/Domain/PublicInterfaces/AnalyzeItems/IItemClass.cs
@@ -21,5 +21,17 @@ namespace CSharpAnalyze.Domain.PublicInterfaces.AnalyzeItems
     /// ジェネリックタイプリスト
     /// </summary>
     List<string> GenericTypes { get; }
+
+    /// <summary>
+    /// 継承元のプロパティリスト
+    /// </summary>
+    /// <remarks>参考情報</remarks>
+    List<string> BaseProperties { get; }
+
+    /// <summary>
+    /// 継承元のメソッドリスト
+    /// </summary>
+    /// <remarks>参考情報</remarks>
+    List<string> BaseMethods { get; }
   }
 }

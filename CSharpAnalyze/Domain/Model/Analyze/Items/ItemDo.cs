265d6336acdd86b1cfedfe9d1968473c1eed2605
kazenetu <rakudafactory@gmail.com>
2019-06-10 21:51:04 +0900

    do-while用クラス・インスタンスを追加

---
+++ CSharpAnalyze/Domain/Model/Analyze/Items/ItemDo.cs
@@ -0,0 +1,98 @@
+﻿using CSharpAnalyze.Domain.Event;
+using CSharpAnalyze.Domain.PublicInterfaces;
+using CSharpAnalyze.Domain.PublicInterfaces.AnalyzeItems;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharpAnalyze.Domain.Model.Analyze.Items
+{
+  /// <summary>
+  /// アイテム：繰り返し:do while
+  /// </summary>
+  internal class ItemDo : AbstractItem, IItenDo
+  {
+    /// <summary>
+    /// 条件
+    /// </summary>
+    public List<IExpression> Conditions { get; } = new List<IExpression>();
+
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    /// <param name="node">対象Node</param>
+    /// <param name="semanticModel">対象ソースのsemanticModel</param>
+    /// <param name="parent">親IAnalyzeItem</param>
+    /// <param name="container">イベントコンテナ</param>
+    public ItemDo(DoStatementSyntax node, SemanticModel semanticModel, IAnalyzeItem parent, EventContainer container) : base(parent, node, semanticModel, container)
+    {
+      ItemType = ItemTypes.MethodStatement;
+
+      // 条件設定
+      var condition = semanticModel.GetOperation(node.Condition);
+      Conditions.AddRange(OperationFactory.GetExpressionList(condition, container));
+
+      // 内部処理設定
+      var block = node.Statement as BlockSyntax;
+      foreach (var statement in block.Statements)
+      {
+        Members.Add(ItemFactory.Create(statement, semanticModel, container, this));
+      }
+    }
+
+    #region 基本インターフェース実装：メソッド
+
+    /// <summary>
+    /// 文字列取得
+    /// </summary>
+    /// <param name="index">前スペース数</param>
+    /// <returns>文字列</returns>
+    public override string ToString(int index = 0)
+    {
+      var result = new StringBuilder();
+      var indexSpace = string.Concat(Enumerable.Repeat("  ", index));
+
+      foreach (var comment in Comments)
+      {
+        result.Append(indexSpace);
+        result.AppendLine($"{comment}");
+      }
+
+      result.Append(indexSpace);
+      result.AppendLine("do{");
+      foreach (var statement in Members)
+      {
+        result.AppendLine(statement.ToString(index + 1));
+      }
+      result.Append(indexSpace);
+      result.AppendLine("}");
+
+      result.Append(indexSpace);
+      result.Append("while(");
+      for (var i = 0; i < Conditions.Count; i++)
+      {
+        var isSetSpace = true;
+        if (i == Conditions.Count - 1)
+        {
+          isSetSpace = false;
+        }
+        else if (Conditions[i].Name == "." || Conditions[i + 1].Name == ".")
+        {
+          isSetSpace = false;
+        }
+        result.Append($"{Conditions[i].Name}");
+        if (isSetSpace)
+        {
+          result.Append(" ");
+        }
+      }
+      result.AppendLine(");");
+
+      return result.ToString();
+    }
+
+    #endregion
+  }
+}
---
+++ CSharpAnalyze/Domain/PublicInterfaces/AnalyzeItems/IItenDo.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace CSharpAnalyze.Domain.PublicInterfaces.AnalyzeItems
+{
+  /// <summary>
+  /// インターフェース:繰り返し do while
+  /// </summary>
+  public interface IItenDo : IAnalyzeItem
+  {
+    /// <summary>
+    /// 条件
+    /// </summary>
+    List<IExpression> Conditions { get; }
+  }
+}

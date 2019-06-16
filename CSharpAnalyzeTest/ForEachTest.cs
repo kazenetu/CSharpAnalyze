6a4cc580ac8c1e287c28a76d1bfa4902211a9f30
kazenetu <rakudafactory@gmail.com>
2019-06-16 23:43:58 +0900

    コレクションの期待値を修正

--- CSharpAnalyzeTest/ForEachTest.cs
+++ CSharpAnalyzeTest/ForEachTest.cs
@@ -214,7 +214,7 @@ namespace CSharpAnalyzeTest
         Assert.Equal("value", GetExpressionsToString(targetInstance.Local));
 
         // コレクション部の型の確認
-        Assert.Equal("Dictionary<int,string>", GetExpressionsToString(targetInstance.CollectionTypes));
+        Assert.Equal("Dictionary<int,string>.Keys", GetExpressionsToString(targetInstance.CollectionTypes));
 
         // コレクション部部の確認
         Assert.Equal("values", GetExpressionsToString(targetInstance.Collection));

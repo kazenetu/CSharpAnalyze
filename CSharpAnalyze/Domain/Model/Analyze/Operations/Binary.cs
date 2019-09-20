97676d6736d5b64677794284a6b0d7bcd42b0851
kazenetu <rakudafactory@gmail.com>
2019-09-20 23:06:30 +0900

    括弧による計算優先順序の設定の不具合を修正

--- CSharpAnalyze/Domain/Model/Analyze/Operations/Binary.cs
+++ CSharpAnalyze/Domain/Model/Analyze/Operations/Binary.cs
@@ -1,5 +1,7 @@
 ﻿using CSharpAnalyze.Domain.Event;
+using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Operations;
+using System;
 
 namespace CSharpAnalyze.Domain.Model.Analyze.Operations
 {
@@ -16,13 +18,56 @@ namespace CSharpAnalyze.Domain.Model.Analyze.Operations
     public Binary(IBinaryOperation operation, EventContainer container) : base(container)
     {
       // 左辺
+      OpenParentheses(operation.LeftOperand);
       Expressions.AddRange(OperationFactory.GetExpressionList(operation.LeftOperand, container));
+      CloseParentheses(operation.LeftOperand);
 
       // 演算子
       Expressions.AddRange(Expression.GetOperationKindExpression(operation));
 
       // 右辺
+      OpenParentheses(operation.RightOperand);
       Expressions.AddRange(OperationFactory.GetExpressionList(operation.RightOperand, container));
+      CloseParentheses(operation.RightOperand);
     }
+
+    /// <summary>
+    /// 開始括弧の確認と追加
+    /// </summary>
+    /// <param name="targetOpration">対象Oprationインスタンス</param>
+    private void OpenParentheses(IOperation targetOpration)
+    {
+      if (targetOpration is IBinaryOperation && ExistsParentheses(targetOpration))
+      {
+        Expressions.Add(new Expression("(", string.Empty));
+      }
+    }
+
+    /// <summary>
+    /// 終了括弧の確認と追加
+    /// </summary>
+    /// <param name="targetOpration">対象Oprationインスタンス</param>
+    private void CloseParentheses(IOperation targetOpration)
+    {
+      if (targetOpration is IBinaryOperation && ExistsParentheses(targetOpration))
+      {
+        Expressions.Add(new Expression(")", string.Empty));
+      }
+    }
+
+    /// <summary>
+    /// 括弧で囲まれているかを返す
+    /// </summary>
+    /// <param name="targetOpration">対象Oprationインスタンス</param>
+    /// <returns>IBinaryOperationなおかつ括弧で囲まれている場合のみtrue</returns>
+    private bool ExistsParentheses(IOperation targetOpration)
+    {
+      if (targetOpration is IBinaryOperation && targetOpration.Syntax.Parent != null)
+      {
+        return $"{targetOpration.Syntax.Parent}".StartsWith("(", StringComparison.CurrentCulture);
+      }
+      return false;
+    }
+
   }
 }
